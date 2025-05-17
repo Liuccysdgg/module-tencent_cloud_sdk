05b12b9afa55637696c119b0333ca3faa597f7a6
nianhua <a1585346868@hotmail.com>
2025-05-17 22:40:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
 # install tencent cloud sdk
-#mkdir 3rdparty -p
-#cd 3rdparty
-#sudo apt-get install libcurl4-openssl-dev
-#sudo apt-get install libssl-dev
-#git clone https://github.com/TencentCloud/tencentcloud-sdk-cpp.git
-#cd tencentcloud-sdk-cpp
-#./build.sh build -DBUILD_MODULES="lcic"
-#./build.sh install
-
+mkdir 3rdparty -p
+cd 3rdparty
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libssl-dev
+git clone https://github.com/TencentCloud/tencentcloud-sdk-cpp.git
+cd tencentcloud-sdk-cpp
+./build.sh build -DBUILD_MODULES="lcic"
+./build.sh install
+cd ../..
 mkdir target -p
 mkdir build -p
 cd build
